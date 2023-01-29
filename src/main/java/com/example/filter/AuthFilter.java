05c8f45be95269fb4359ad3c7b492d6cc643827b
Sergey <spapoyan1987@gmail.com>
2023-01-29 19:56:16 +0400

    commit

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,20 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/user/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        Object user = httpServletRequest.getSession().getAttribute("user");
+        if (user == null) {
+            ((HttpServletResponse) servletResponse).sendRedirect("login.jsp");
+        } else filterChain.doFilter(servletRequest, servletResponse);
+
+    }
 }
